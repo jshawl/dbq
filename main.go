1cf15475f9bbdaf632ce9ab488dec9dc98a96255
Jesse Shawl <jesse@jesse.sh>
2025-09-17 20:33:46 -0400

    lint (#3)

--- main.go
+++ main.go
@@ -1,40 +1,46 @@
+// package main implements a query tool for databases
 package main
 
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
 	ctx := context.Background()
+
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
 		log.Fatal("DATABASE_URL is not set")
 	}
+
 	pgdb, err := NewPostgresDB(ctx, dsn)
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
-	db := NewDB(pgdb)
+
+	database := NewDB(pgdb)
 
 	defer func() {
-		if err := db.Close(ctx); err != nil {
+		err := database.Close(ctx)
+		if err != nil {
 			log.Fatalf("Failed to close database connection: %v", err)
 		}
 	}()
 
-	query, err := db.Query(ctx, "select * from users limit 2;")
+	query, err := database.Query(ctx, "select * from users limit 2;")
 	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("success in %s\n", query.Duration)
-	jsonData, err := json.MarshalIndent(query.Results, "", "  ")
-	if err != nil {
-		log.Fatalf("Failed to format results: %v", err)
+		log.Println(err)
 	}
 
-	fmt.Println(string(jsonData))
+	log.Printf("success in %s\n", query.Duration)
+
+	jsonData, err := json.MarshalIndent(query.Results, "", "  ")
+	if err != nil {
+		log.Printf("Failed to format results: %v", err)
+	}
+
+	log.Println(string(jsonData))
 }
