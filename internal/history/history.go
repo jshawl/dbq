b2c458c0ede1e044c12bab4becae42dd453cef66
Jesse Shawl <jesse@jesse.sh>
2025-09-20 14:57:58 -0400

    add history (#10)

---
+++ internal/history/history.go
@@ -0,0 +1,191 @@
+package history
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type history struct {
+	Value  string
+	cursor int64
+	db     *sql.DB
+}
+
+func Init(path string) history {
+	_, errStat := os.Stat(path)
+
+	database, err := sql.Open("sqlite3", path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if errStat != nil {
+		sqlStmt := `
+		create table history (
+			id integer not null primary key,
+			query text,
+			created_at datetime default current_timestamp
+		);
+	`
+
+		_, err := database.ExecContext(context.Background(), sqlStmt)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return history{
+		cursor: 0,
+		db:     database,
+		Value:  "",
+	}
+}
+
+func (h history) Cleanup() {
+	defer func() {
+		err := h.db.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+}
+
+type PushMsg struct {
+	Entry string
+}
+
+type PushedMsg struct {
+	id int64
+}
+
+type TravelMsg struct {
+	Direction string
+}
+
+type TraveledMsg struct {
+	cursor int64
+	value  string
+}
+
+func (model history) Update(msg tea.Msg) (history, tea.Cmd) {
+	switch msg := msg.(type) {
+	case PushMsg:
+		entry := msg.Entry
+
+		return model, model.push(entry)
+	case PushedMsg:
+		cursor := msg.id
+		model.cursor = cursor
+
+		return model, nil
+	case TravelMsg:
+		return model, model.travel(msg.Direction)
+
+	case TraveledMsg:
+		model.cursor = msg.cursor
+		model.Value = msg.value
+
+		return model, nil
+	}
+
+	return model, nil
+}
+
+func (model history) push(entry string) tea.Cmd {
+	return func() tea.Msg {
+		transaction, err := model.db.BeginTx(
+			context.Background(),
+			&sql.TxOptions{ReadOnly: false, Isolation: 0},
+		)
+		if err != nil {
+			log.Fatal("db begin err")
+		}
+
+		stmt, err := transaction.PrepareContext(
+			context.Background(),
+			"insert into history (query) values (?)",
+		)
+		if err != nil {
+			log.Fatal("prepare err")
+		}
+
+		result, _ := stmt.ExecContext(context.Background(), entry)
+
+		err = transaction.Commit()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		lastInsertId, err := result.LastInsertId()
+		if err != nil {
+			log.Fatal("exec err")
+		}
+
+		defer func() {
+			err := stmt.Close()
+			if err != nil {
+				panic(err)
+			}
+		}()
+
+		return PushedMsg{
+			id: lastInsertId,
+		}
+	}
+}
+
+func (model history) travel(direction string) tea.Cmd {
+	return func() tea.Msg {
+		_, err := model.db.BeginTx(
+			context.Background(),
+			&sql.TxOptions{ReadOnly: true, Isolation: 0},
+		)
+		if err != nil {
+			log.Fatal("db begin err")
+		}
+
+		var sql string
+		if direction == "next" {
+			sql = "select id, query from history where id > (?) order by id asc limit 1;"
+		}
+
+		if direction == "previous" {
+			sql = "select id, query from history where id < (?) order by id desc limit 1;"
+		}
+
+		stmt, _ := model.db.PrepareContext(
+			context.Background(),
+			sql,
+		)
+
+		defer func() {
+			err := stmt.Close()
+			if err != nil {
+				panic(err)
+			}
+		}()
+
+		var query string
+
+		//nolint:varnamelen
+		var id int64
+
+		err = stmt.QueryRowContext(context.Background(), model.cursor).Scan(&id, &query)
+		if err != nil {
+			return TraveledMsg{
+				cursor: model.cursor,
+				value:  model.Value,
+			}
+		}
+
+		return TraveledMsg{
+			cursor: id,
+			value:  query,
+		}
+	}
+}
---
+++ internal/history/history_test.go
@@ -0,0 +1,216 @@
+package history_test
+
+import (
+	"os"
+	"testing"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/jshawl/dbq/internal/history"
+)
+
+//nolint:ireturn
+func assertMsgType[T interface{}](t *testing.T, cmd tea.Cmd) T {
+	t.Helper()
+
+	if cmd == nil {
+		t.Fatalf("%T cmd is nil ", new(T))
+	}
+
+	msg := cmd()
+	typed, ok := msg.(T)
+
+	if !ok {
+		t.Fatalf("Expected msg to be of type %T, got %T", *new(T), msg)
+	}
+
+	return typed
+}
+
+func setupHistoryStore(t *testing.T) string {
+	t.Helper()
+
+	return t.TempDir()
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+
+	return err == nil
+}
+
+func TestInit(t *testing.T) {
+	t.Parallel()
+
+	t.Run("creates a sqlite db", func(t *testing.T) {
+		t.Parallel()
+
+		dir := setupHistoryStore(t)
+
+		path := dir + "/foo.db"
+		if fileExists(path) {
+			t.Fatal("expected db not to be created")
+		}
+
+		h := history.Init(path)
+
+		h.Cleanup()
+
+		if !fileExists(path) {
+			t.Fatal("expected db to be created")
+		}
+	})
+
+	t.Run("does not overwrite existing db", func(t *testing.T) {
+		t.Parallel()
+
+		dir := setupHistoryStore(t)
+		path := dir + "/foo.db"
+		h1 := history.Init(path)
+		h2 := history.Init(path)
+
+		h1.Cleanup()
+		h2.Cleanup()
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Parallel()
+
+	t.Run("PushMsg", func(t *testing.T) {
+		t.Parallel()
+
+		var cmd tea.Cmd
+
+		dir := setupHistoryStore(t)
+		path := dir + "/foo.db"
+		hist := history.Init(path)
+		hist, cmd = hist.Update(history.PushMsg{Entry: "select * from users limit 1;"})
+		assertMsgType[history.PushedMsg](t, cmd)
+
+		hist.Cleanup()
+	})
+
+	t.Run("TravelMsg(previous) does not update the Value if no results", func(t *testing.T) {
+		t.Parallel()
+
+		var cmd tea.Cmd
+
+		dir := setupHistoryStore(t)
+		path := dir + "/foo.db"
+
+		hist := history.Init(path)
+
+		hist, cmd = hist.Update(history.TravelMsg{Direction: "previous"})
+		hist, _ = hist.Update(assertMsgType[history.TraveledMsg](t, cmd))
+
+		if hist.Value != "" {
+			t.Fatalf("expected empty string, got %s", hist.Value)
+		}
+
+		hist.Cleanup()
+	})
+
+	t.Run("TravelMsg(previous) does not update the Value if one result", func(t *testing.T) {
+		t.Parallel()
+
+		var cmd tea.Cmd
+
+		dir := setupHistoryStore(t)
+		path := dir + "/foo.db"
+
+		hist := history.Init(path)
+		hist, cmd = hist.Update(history.PushMsg{Entry: "select * from users limit 1;"})
+		hist, _ = hist.Update(assertMsgType[history.PushedMsg](t, cmd))
+
+		hist, cmd = hist.Update(history.TravelMsg{Direction: "previous"})
+		hist, _ = hist.Update(assertMsgType[history.TraveledMsg](t, cmd))
+
+		if hist.Value != "" {
+			t.Fatalf("expected empty string, got %s", hist.Value)
+		}
+
+		hist.Cleanup()
+	})
+
+	t.Run("TravelMsg(previous) sets the Value to the penultimate row", func(t *testing.T) {
+		t.Parallel()
+
+		dir := setupHistoryStore(t)
+		path := dir + "/foo.db"
+
+		hist := history.Init(path)
+
+		var cmd tea.Cmd
+
+		hist, cmd = hist.Update(history.PushMsg{Entry: "select * from users limit 1;"})
+		hist, _ = hist.Update(assertMsgType[history.PushedMsg](t, cmd))
+
+		hist, cmd = hist.Update(history.PushMsg{Entry: "select * from users limit 2;"})
+		hist, _ = hist.Update(assertMsgType[history.PushedMsg](t, cmd))
+
+		hist, cmd = hist.Update(history.PushMsg{Entry: "select * from users limit 3;"})
+		hist, _ = hist.Update(assertMsgType[history.PushedMsg](t, cmd))
+
+		hist, cmd = hist.Update(history.TravelMsg{Direction: "previous"})
+		hist, _ = hist.Update(assertMsgType[history.TraveledMsg](t, cmd))
+
+		if hist.Value != "select * from users limit 2;" {
+			t.Fatalf("expected current, got %s", hist.Value)
+		}
+
+		hist.Cleanup()
+	})
+
+	t.Run("TravelMsg(next) sets the Value to the next row", func(t *testing.T) {
+		t.Parallel()
+
+		dir := setupHistoryStore(t)
+		path := dir + "/foo.db"
+
+		hist := history.Init(path)
+
+		var cmd tea.Cmd
+
+		hist, cmd = hist.Update(history.PushMsg{Entry: "select * from users limit 1;"})
+		hist, _ = hist.Update(assertMsgType[history.PushedMsg](t, cmd))
+
+		hist, cmd = hist.Update(history.PushMsg{Entry: "select * from users limit 2;"})
+		hist, _ = hist.Update(assertMsgType[history.PushedMsg](t, cmd))
+
+		hist, cmd = hist.Update(history.TravelMsg{Direction: "previous"})
+		hist, _ = hist.Update(assertMsgType[history.TraveledMsg](t, cmd))
+
+		if hist.Value != "select * from users limit 1;" {
+			t.Fatalf("expected current, got %s", hist.Value)
+		}
+
+		hist, cmd = hist.Update(history.TravelMsg{Direction: "next"})
+		hist, _ = hist.Update(assertMsgType[history.TraveledMsg](t, cmd))
+
+		if hist.Value != "select * from users limit 2;" {
+			t.Fatalf("expected current, got %s", hist.Value)
+		}
+
+		hist.Cleanup()
+	})
+
+	t.Run("TravelMsg(next) does not update the Value if no results", func(t *testing.T) {
+		t.Parallel()
+
+		dir := setupHistoryStore(t)
+		path := dir + "/foo.db"
+
+		hist := history.Init(path)
+
+		var cmd tea.Cmd
+
+		hist, cmd = hist.Update(history.TravelMsg{Direction: "next"})
+		hist, _ = hist.Update(assertMsgType[history.TraveledMsg](t, cmd))
+
+		if hist.Value != "" {
+			t.Fatalf("expected empty string, got %s", hist.Value)
+		}
+
+		hist.Cleanup()
+	})
+}
