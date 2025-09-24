dfe30c1255ecf6bc7493ce8d3ec6bb54067ffa61
Jesse Shawl <jesse@jesse.sh>
2025-09-24 05:28:18 -0400

    debug log

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -36,19 +35,19 @@ type QueryResponseReceivedMsg struct {
 }
 
 func Run() {
-	if len(os.Getenv("DEBUG")) > 0 {
-		f, _ := tea.LogToFile("debug.log", "debug")
 
-		defer func() {
-			err := f.Close()
-			if err != nil {
-				panic(err)
-			}
-		}()
-	}
+	f, _ := tea.LogToFile("debug.log", "debug")
+
+	defer func() {
+		err := f.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
 
 	p := tea.NewProgram(InitialModel(), tea.WithAltScreen(), tea.WithMouseCellMotion())
 
+	log.Println("ui.Run()")
 	_, err := p.Run()
 	if err != nil {
 		defer func() {
