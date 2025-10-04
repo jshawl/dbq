212a810ac68d75024dfd41ef819e826fa3ede7ac
Jesse Shawl <jesse@jesse.sh>
2025-10-04 16:15:23 -0400

    fix sha

--- Formula/dbq.rb
+++ Formula/dbq.rb
@@ -1,7 +1,7 @@
 class Dbq < Formula
   desc "a database query tui"
   url "https://github.com/jshawl/dbq/archive/refs/tags/v2025.10.04.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "266b7e6f7aa8fcad0058432cf5b4a43ecbf99dfac0784fead9a4db8112b92211"
 
   depends_on "go" => :build
 
